f7fe1a2021be8da20a13f1cbeffd29f82ccabc8e
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-05-11 22:00:00 -0200

    Typo

--- extensions/systemd-resolved_resolvconf.sh
+++ extensions/systemd-resolved_resolvconf.sh
@@ -2,6 +2,6 @@ function pre_prepare_partitions__set_systemd-resolved(){
 	display_alert "Asking NetworkManager to not handle /etc/resolv.conf" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
 	run_host_command_logged sed -i 's/^dns\=.*/dns\=none/g' "${SDCARD}"/etc/NetworkManager/NetworkManager.conf
 	run_host_command_logged sed -i 's/rc-manager\=.*/rc-manager\=unmanaged/g' "${SDCARD}"/etc/NetworkManager/NetworkManager.conf
-        display_alert "Cleaning deboostrapped resolv.conf -systemd-resolved to take over" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
+        display_alert "Cleaning deboostrapped resolv.conf - systemd-resolved to take over" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
         run_host_command_logged rm -vf "${SDCARD}"/etc/resolv.conf
 }
