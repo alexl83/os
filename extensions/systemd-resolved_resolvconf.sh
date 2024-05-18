a69f90ba1d80f0cabe77a02c2af965b1d9369ade
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-05-17 22:00:00 -0200

    Cosmetic fix

--- extensions/systemd-resolved_resolvconf.sh
+++ extensions/systemd-resolved_resolvconf.sh
@@ -1,5 +1,5 @@
 function post_post_debootstrap_tweaks__250_set_systemd-resolved(){
-	display_alert "Asking NetworkManager to not handle /etc/resolv.conf" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
+	display_alert "Asking NetworkManager not to handle /etc/resolv.conf" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
 	run_host_command_logged sed -i 's/^dns\=.*/dns\=none/g' "${SDCARD}"/etc/NetworkManager/NetworkManager.conf
 	run_host_command_logged sed -i 's/rc-manager\=.*/rc-manager\=unmanaged/g' "${SDCARD}"/etc/NetworkManager/NetworkManager.conf
         display_alert "Cleaning deboostrapped resolv.conf - systemd-resolved to take over" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
