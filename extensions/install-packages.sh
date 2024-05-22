3496b0a15be1c99f7c93ad19a9c244874978976d
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-05-22 23:59:41 +0200

    close if statement

--- extensions/install-packages.sh
+++ extensions/install-packages.sh
@@ -3,8 +3,8 @@ function pre_customize_image__251_install_custom_packages(){
 	pkgs="net-tools moreutils byobu git dkms gpsd zsh-autosuggestions macchanger avahi-daemon vnstat xauth gpsd-tools libnss-mdns pwgen zerotier-one rfkill bluetooth bluez bluez-tools lirc"
 	rem_pkgs="ifupdown"
 	if [ "${BOARD}" == "orangepi5-plus" ]; then
-	rem_pkgs+="fake-hwclock"
-
+		rem_pkgs+="fake-hwclock"
+	fi	
 	display_alert "Updating package list" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
 	do_with_retries 3 chroot_sdcard_apt_get_update
 
