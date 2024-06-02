730d7153ee81a07711210db52b6858ebedee9d3c
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-06-03 01:43:47 +0200

    tweaks for opi02w leds and restore removal of ifupdown

--- extensions/install-packages.sh
+++ extensions/install-packages.sh
@@ -1,7 +1,7 @@
 function pre_customize_image__251_install_custom_packages(){
 
 	pkgs=(net-tools moreutils byobu git dkms gpsd zsh-autosuggestions macchanger avahi-daemon vnstat xauth gpsd-tools libnss-mdns pwgen zerotier-one rfkill bluetooth bluez bluez-tools lirc)
-	rem_pkgs=()
+	rem_pkgs=(ifupdown)
 	if [ "${BOARD}" == "orangepi5-plus" ]; then
 		rem_pkgs+=(fake-hwclock)
 	fi	
