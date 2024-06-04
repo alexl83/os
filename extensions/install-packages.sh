d94d4b4425138a80b0f557ebf06c06e4c44f2d81
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-06-04 19:12:08 +0200

    remove keyboard-configuration as it's not needed on a headless machine

--- extensions/install-packages.sh
+++ extensions/install-packages.sh
@@ -1,7 +1,7 @@
 function pre_customize_image__251_install_custom_packages(){
 
 	pkgs=(net-tools moreutils byobu git dkms gpsd zsh-autosuggestions macchanger avahi-daemon vnstat xauth gpsd-tools libnss-mdns pwgen zerotier-one rfkill bluetooth bluez bluez-tools lirc libpam-google-authenticator)
-	rem_pkgs=(ifupdown)
+	rem_pkgs=(ifupdown keyboard-configuration)
 	if [ "${BOARD}" == "orangepi5-plus" ]; then
 		rem_pkgs+=(fake-hwclock)
 	fi	
