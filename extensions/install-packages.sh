36e2fc7ac0d41e3db11a1a9be82155513e889aa3
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-06-13 00:16:54 +0200

    tweak package removal logic

--- extensions/install-packages.sh
+++ extensions/install-packages.sh
@@ -2,9 +2,12 @@ function pre_customize_image__251_install_custom_packages(){
 
 	pkgs=(net-tools moreutils byobu git dkms gpsd zsh-autosuggestions macchanger avahi-daemon vnstat xauth gpsd-tools libnss-mdns pwgen zerotier-one rfkill bluetooth bluez bluez-tools lirc libpam-google-authenticator mtd-utils)
 	rem_pkgs=(ifupdown keyboard-configuration exim4-daemon-light)
-	if [ "${BOARD}" == "orangepi5-plus" ]; then
-		rem_pkgs+=(fake-hwclock)
-	fi	
+	rtc_board=(orangepi5-plus nanopi-r5c)
+	for rtc_match in "${rtc_board[@]}"; do
+		if [ "${BOARD}" == "${rtc_match}" ]; then
+			rem_pkgs+=(fake-hwclock)
+		fi
+	done
 	display_alert "Updating package list" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
 	do_with_retries 3 chroot_sdcard_apt_get_update
 
