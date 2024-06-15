caf78534d34bf793dbe03430d01eb2dd7460747d
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-06-15 02:00:00 +0200

    fix syncthing repo key path

--- extensions/syncthing.sh
+++ extensions/syncthing.sh
@@ -6,7 +6,7 @@ function pre_customize_image__250_3_install_syncthing_repositories() {
 	# Add sources.list
 	if [[ "${DISTRIBUTION}" == "Debian" ]]; then
 		display_alert "Adding sources.list for Syncthing" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
-		run_host_command_logged echo "deb [arch="${ARCH}" signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" "|" tee "${SDCARD}"/etc/apt/sources.list.d/syncthing.list
+		run_host_command_logged echo "deb [arch="${ARCH}" signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" "|" tee "${SDCARD}"/etc/apt/sources.list.d/syncthing.list
 		display_alert "Pinning Syncthing package versions to apt for consistency" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
 		run_host_command_logged cat <<- 'end' > "${SDCARD}"/etc/apt/preferences.d/syncthing
 			Package: *
