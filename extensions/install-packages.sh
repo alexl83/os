6ff5498d864f0b1ed0ca64a55709642b30c21d18
Alessandro Lannocca <alessandro.lannocca@gmail.com>
2024-06-23 11:00:13 +0100

    remove a couple of unused packages

--- extensions/install-packages.sh
+++ extensions/install-packages.sh
@@ -1,7 +1,7 @@
 function pre_customize_image__251_install_custom_packages(){
 
 	pkgs=(net-tools moreutils byobu git dkms gpsd zsh-autosuggestions macchanger avahi-daemon vnstat xauth gpsd-tools libnss-mdns pwgen zerotier-one rfkill bluetooth bluez bluez-tools libpam-google-authenticator mtd-utils device-tree-compiler dnsmasq-base)
-	rem_pkgs=(ifupdown keyboard-configuration exim4-daemon-light haveged)
+	rem_pkgs=(ifupdown keyboard-configuration exim4-daemon-light haveged armbian-plymouth-theme armbian-zsh)
 	rtc_board=(orangepi5-plus-kali nanopi-r5c)
 	ir_board=(orangepi5-plus-kali orangepizero3 orangepizero2w)
 	for rtc_match in "${rtc_board[@]}"; do
